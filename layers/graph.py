a59b4cdb719dcf26631a8bd07a6c6d227c6fb229
adimyth <mishraaditya6991@gmail.com>
2020-10-08 16:48:23 +0530

    feat(graph): ports to tf2.2

--- layers/graph.py
+++ layers/graph.py
@@ -9,14 +9,11 @@ def matmul(x, y, sparse=False):
 
 
 class GraphConvLayer(tf.keras.layers.Layer):
-    def __init__(
-        self, input_dim, output_dim, activation=None, use_bias=False, name="graph_conv"
-    ):
+    def __init__(self, output_dim, activation=None, use_bias=False, name="graph_conv"):
         super(GraphConvLayer, self).__init__()
         """Initialise a Graph Convolution layer.
 
         Args:
-            input_dim (int): The input dimensionality.
             output_dim (int): The output dimensionality, i.e. the number of
                 units.
             activation (callable): The activation function to use. Defaults to
@@ -24,25 +21,27 @@ class GraphConvLayer(tf.keras.layers.Layer):
             use_bias (bool): Whether to use bias or not. Defaults to `False`.
             name (str): The name of the layer. Defaults to `graph_conv`.
         """
-        self.input_dim = input_dim
         self.output_dim = output_dim
         self.activation = activation
         self.use_bias = use_bias
 
-        with tf.name_scope(self.name):
-            self.w = self.add_weight(
-                name="w",
-                shape=(self.input_dim, self.output_dim),
-                initializer=tf.initializers.glorot_uniform(),
+    def build(self, input_shape):
+        self.input_dim = input_shape[1][-1]
+        self.w = self.add_weight(
+            name="w",
+            shape=(self.input_dim, self.output_dim),
+            initializer=tf.initializers.glorot_uniform(),
+        )
+
+        if self.use_bias:
+            self.b = tf.add_weight(
+                name="b", initializer=tf.constant(0.1, shape=(self.output_dim,))
             )
 
-            if self.use_bias:
-                self.b = tf.add_weight(
-                    name="b", initializer=tf.constant(0.1, shape=(self.output_dim,))
-                )
-
-    def call(self, adj_norm, x, sparse=False):  # A=D̃⁻¹/²ÃD̃⁻¹/²
-        x = matmul(x=x, y=self.w, sparse=sparse)  # XW
+    def call(self, inputs):
+        adj_norm = inputs[0]
+        x = inputs[1]
+        x = matmul(x=x, y=self.w, sparse=False)  # XW
         x = matmul(x=adj_norm, y=x, sparse=True)  # AXW
 
         if self.use_bias:
